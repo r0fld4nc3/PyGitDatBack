614e8e95e3f101ee978b2b7c96f8f1e6bfe2923f
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2024-10-28 00:37:16 +0100

    Fix ticks. Doesn't work

--- launch.sh
+++ launch.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 SESSION_USER="$(whoami)"
-WORK_DIR="/home/'$SESSION_USER'/.local/share/r0fld4nc3/PyGitDatBack"
-VENV_PATH="'$WORK_DIR'/PyGitDatBack.venv"
+WORK_DIR="/home/"$SESSION_USER"/.local/share/r0fld4nc3/PyGitDatBack"
+VENV_PATH=""$WORK_DIR"/PyGitDatBack.venv"
 THIS_DIR="$(pwd)"
-REQUIREMENTS="'$THIS_DIR'/src/requirements.txt"
+REQUIREMENTS=""$THIS_DIR"/src/requirements.txt"
 
-echo "Creating virtual environment at '$VENV_PATH'"
+echo "Creating virtual environment at "$VENV_PATH""
 
 python3 -m venv "$VENV_PATH" 
 source "$VENV_PATH/bin/activate"
 pip install -r "$REQUIREMENTS"
-python3 "'$THIS_DIR'/src/main.py"
+python3 ""$THIS_DIR"/src/main.py"
