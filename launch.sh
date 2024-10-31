27b31d0979075eafedb549ec0ff89cb7e9b8284d
Artur <41012911+r0fld4nc3@users.noreply.github.com>
2024-10-31 20:08:59 +0100

    Update launch.sh
    - Fixed "THIS_DIR" path to not be the "print working directory" but instead use a one liner from https://stackoverflow.com/a/246128 to return the path of the file, no matter where it is called from.

--- launch.sh
+++ launch.sh
@@ -3,7 +3,7 @@
 SESSION_USER="$(whoami)"
 WORK_DIR="/home/"$SESSION_USER"/.local/share/r0fld4nc3/PyGitDatBack"
 VENV_PATH=""$WORK_DIR"/PyGitDatBack.venv"
-THIS_DIR="$(pwd)"
+THIS_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd ) #h ttps://stackoverflow.com/a/246128
 REQUIREMENTS=""$THIS_DIR"/src/requirements.txt"
 
 echo "Creating virtual environment at "$VENV_PATH""
